38ec79fed878816055725a375b349c93a38a4ce7
Luke <luke@localhost.localdomain>
2015-01-20 13:44:29 -0700

    Removed unused imports, and random netbeans header.

--- Globals.py
+++ Globals.py
@@ -1,9 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-import sys, os
-
-
 currentTime = 0 # The current global time
 objectNames = None # Hands out unique name to every panda object
 eventSignals = {}
