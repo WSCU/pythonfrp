686811e515b657358db223197bfbd10fc7262ec4
Asher Holloman <Asher Holloman>
2016-01-21 15:25:34 -0700

    Added wrapper for reactive user imports
    Consolidated the user-level imports (everything but the Engine) into a
    single file known as ReactiveUser.py

---
+++ ReactiveUser.py
@@ -0,0 +1,9 @@
+from pythonfrp.StaticNumerics import *
+import pythonfrp.Proxy as Proxy
+from pythonfrp.Functions import *
+from pythonfrp.Numerics import *
+import pythonfrp.Globals as Globals
+from pythonfrp.Factory import *
+
+#consolidation of user level imports for Reactive Engine
+#from pythonfrp.ReactiveUser import * 
