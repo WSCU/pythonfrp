1756652aa0681792ca51a210cb31a95d64bde1fd
unknown <stu340407@H101-16.wsc.western.edu>
2016-02-04 15:06:48 -0700

    Fixed Error with updateWorld
    There was an error occurring with updateWorld due to its positioning. I
    simply moved it to the proper positon

--- World.py
+++ World.py
@@ -3,6 +3,10 @@ from . import Globals
 from . Types import *
 from . import Numerics
 
+def updateWorld(self):
+    for f in world._updaters:
+        f(self)
+
 class World(Proxy.Proxy):
     def __init__(self, update = updateWorld):
         Proxy.Proxy.__init__(self, "world", update, {})
@@ -14,12 +18,8 @@ def addSignal(name,default,type,update): #Add a signal to a given object, paired
     world.__setattr__(name,default)
     world._types[name]= type
     world._updaters.add(update)
-    
-def updateWorld(self):
-    for f in world._updaters:
-        f(self)
 
 def resetWorld(continueFn = lambda: None):
     Globals.resetFlag=continueFn
-    # Should make all DirectGUI stuff invisible
+    #Makes all DirectGUI stuff invisible
 
