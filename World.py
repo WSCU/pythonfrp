624e0d2d5830cf3836514a2bbce7bf7859595044
Miller Ridgeway <millerr2017@gmail.com>
2016-02-04 21:58:11 -0700

    Did a little formatting and fixed the list function append; it was previously 'add'. Still can't get the addSignal notation to work, but I will keep at it.

--- World.py
+++ World.py
@@ -1,25 +1,29 @@
 from . import Proxy
 from . import Globals
-from . Types import *
+from .Types import *
 from . import Numerics
+
 
 def updateWorld(self):
     for f in world._updaters:
         f(self)
 
+
 class World(Proxy.Proxy):
-    def __init__(self, update = updateWorld):
+    def __init__(self, update=updateWorld):
         Proxy.Proxy.__init__(self, "world", update, {})
+        # Create a world labeled 'world' with an updater that calls each signal's custom update
+
 
 world = World()
 world._updaters = []
 
-def addSignal(name,default,type,update): #Add a signal to a given object, paired with a specified updater
-    world.__setattr__(name,default)
-    world._types[name]= type
-    world._updaters.add(update)
 
-def resetWorld(continueFn = lambda: None):
-    Globals.resetFlag=continueFn
-    #Makes all DirectGUI stuff invisible
+def addSignal(name, default, type, update):  # Add a signal to a given object, paired with a specified updater.
+    world.__setattr__(name, default)
+    world._types[name] = type
+    world._updaters.append(update)
 
+
+def resetWorld(continueFn=lambda: None):  # Makes all DirectGUI stuff invisible
+    Globals.resetFlag = continueFn
